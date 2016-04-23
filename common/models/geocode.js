d9df4550facc39e3fa6644fa2945d36ecfd6356b
jirishus <jirishus@yahoo.com>
2016-04-23 10:33:43 -0700

    Geocode model remote method setup

--- common/models/geocode.js
+++ common/models/geocode.js
@@ -1,3 +1,19 @@
-module.exports = function(Geocode) {
+module.exports = (Geocode) => {
+
+  // define remote method
+  Geocode.locate = (data) => {
+    return new Promise((resolve, reject) => {
+
+      resolve(data);
+
+    });
+  }
+
+  // expose remote method
+  Geocode.remoteMethod('locate', {
+    accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+    returns: { arg: 'data', type: 'object', root: true },
+    http: { path: '/locate', verb: 'post' }
+  });
 
 };
